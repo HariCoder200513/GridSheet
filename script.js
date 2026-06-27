a53dc46f96f07ebc66960c8c9c2aa319c7e5e021
HariCoder200513 <harivigneshwaran2005@gmail.com>
2026-06-27 21:59:31 +0530

    Centered First Row Headers

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
-const ROW = 100;
-const COLUMN = 26;
+const ROW = 101;
+const COLUMN = 27;
 const grid = document.querySelector(".grid");
 
+const horizontal = document.querySelector(".horizontal");
+const vertical = document.querySelector(".vertical");
 
 
 for (let i = 0; i < ROW; i++) {
   for (let j = 0; j < COLUMN; j++) {
     const cell = document.createElement("div");
     cell.addEventListener("click", () => {
-      cell.style.border = "1px solid #089949";
+      cell.style.border = "2px solid #089949";
     });
+
+    if(i==0)
+    {
+      cell.textContent = String.fromCharCode(65 + j);
+      cell.style.textAlign = "center";
+      
+    }
 
     cell.classList.add("cell");
     grid.appendChild(cell);
