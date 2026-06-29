ef0fc80da476307a423652b59580ff742cad76ba
HariCoder200513 <harivigneshwaran2005@gmail.com>
2026-06-29 17:47:31 +0530

    Removed EventListener from nested for loop

--- script.js
+++ script.js
@@ -2,24 +2,24 @@ const ROW = 101;
 const COLUMN = 27;
 const grid = document.querySelector(".grid");
 
-const horizontal = document.querySelector(".horizontal");
-const vertical = document.querySelector(".vertical");
 
-
+// cell.addEventListener("click",()=>{
+//   cell.style.border = "2px solid #089949";
+// })
 
 for (let i = 0; i < ROW; i++) {
   for (let j = 0; j < COLUMN; j++) {
     const cell = document.createElement("div");
-    cell.addEventListener("click", () => {
-      cell.style.border = "2px solid #089949";
-    });
+    // cell.addEventListener("click", () => {
+    //   cell.style.border = "2px solid #089949";
+    // });
 
-    if (i == 0&&j>0) {
-      cell.textContent = String.fromCharCode(65 + j-1);
+    if (i == 0 && j > 0) {
+      cell.textContent = String.fromCharCode(65 + j - 1);
       cell.style.textAlign = "center";
     }
 
-    if (j == 0&&i>0) {
+    if (j == 0 && i > 0) {
       cell.textContent = i;
       cell.style.textAlign = "center";
     }
@@ -28,3 +28,12 @@ for (let i = 0; i < ROW; i++) {
     grid.appendChild(cell);
   }
 }
+
+const cell = document.querySelectorAll(".cell");
+console.log(cell);
+cell.forEach((c) => {
+  c.addEventListener("click", () => {
+    c.style.border = "2px solid #089949";
+  });
+});
+
