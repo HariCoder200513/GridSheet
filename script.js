5281f2a83654a8273848ba535b8d513726d22eb5
HariCoder200513 <harivigneshwaran2005@gmail.com>
2026-06-30 17:56:47 +0530

    Added Sticky Nav to Top and Left

--- script.js
+++ script.js
@@ -5,10 +5,23 @@ const grid = document.querySelector(".grid");
 for (let i = 0; i < ROW; i++) {
   for (let j = 0; j < COLUMN; j++) {
     const cell = document.createElement("div");
-
     if (i == 0 && j > 0) {
       cell.textContent = String.fromCharCode(65 + j - 1);
       cell.style.textAlign = "center";
+    }
+
+    if(i==0)
+    {
+      cell.style.position="sticky";
+      cell.style.top="0";
+      cell.style.backgroundColor="#f1f1f1";
+    }
+
+    if(j==0)
+    {
+      cell.style.position="sticky";
+      cell.style.left="0";
+      cell.style.backgroundColor="#f1f1f1";
     }
 
     if (j == 0 && i > 0) {
@@ -16,7 +29,7 @@ for (let i = 0; i < ROW; i++) {
       cell.style.textAlign = "center";
     }
 
-    if(i!=0&&j!=0)
+    if(i>0&&j>0)
     {
       cell.setAttribute("contenteditable", "true");
     }
@@ -25,6 +38,9 @@ for (let i = 0; i < ROW; i++) {
     grid.appendChild(cell);
   }
 }
+
+
+
 
 let previous=null;
 grid.addEventListener("click",(e)=>{
